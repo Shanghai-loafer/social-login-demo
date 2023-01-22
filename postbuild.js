fe5b025c92b31442f6bf3b3039b19dd80e5c8e81
t-adachi <t-adachi@seaos.co.jp>
2023-01-22 17:06:54 +0900

    staticディレクトリ内もコミット対象から外すよう修正

--- postbuild.js
+++ postbuild.js
@@ -2,10 +2,19 @@ const path = require('path');
 const fs = require('fs-extra');
 
 const BUILD_DIR = path.join(__dirname, './build');
-const PUBLIC_DIR = path.join(__dirname, '../src/main/resources/templates/build');
+const PUBLIC_DIR = path.join(__dirname, '../src/main/resources/templates/public');
+
+const DELETE_STATIC_DIR = path.join(__dirname, '../src/main/resources/templates/public/static');
+
+const STATIC_DIR_SRC = path.join(__dirname, './build/static');
+const STATIC_DIR_DEST = path.join(__dirname, '../src/main/resources/static');
 
 fs.emptyDirSync(PUBLIC_DIR);
 fs.copySync(BUILD_DIR, PUBLIC_DIR);
+fs.remove(DELETE_STATIC_DIR);
+
+fs.emptyDirSync(STATIC_DIR_DEST);
+fs.copySync(STATIC_DIR_SRC, STATIC_DIR_DEST);
 
 /**
  * emptyDirSync()関数は、emptyDir()関数の同期版です。
